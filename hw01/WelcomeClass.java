b353388d34b5c52527ef22db2cd5dea88ea605c6
zhl218 <zhl218@lehigh.edu>
2015-08-31 15:08:18 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Zhenyu Li 
+//Welcome Class Java Program 
+
+
+
+
+
+public class WelcomeClass {
+
+public static void main (String[] args){
+
+System.out.println("    -----------"); 
+System.out.println("    | WELCOME |"); 
+System.out.println("    -----------"); 
+System.out.println("  ^  ^  ^  ^  ^  ^"); 
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+System.out.println("<-Z--H--L--2--1--8->");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println("  v  v  v  v  v  v"); 
+System.out.println("I don't like peanut butter."); 
+}
+}
+
---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+//Zhenyu Li 
+//Hello World Java Program 
+// 
+//  first compile the program 
+//      javac HellWorld.java 
+//  run the program 
+//      java HelloWorld// 
+
+//  define a class 
+public class HelloWorld{
+    
+//  add main method 
+ public static void main(String[] args){
+     
+// print the statement with box around it 
+    System.out.println("########################"); 
+    System.out.println("########################"); 
+    System.out.println("#                      #"); 
+    System.out.println("#  Hello World 2015!!  #"); 
+    System.out.println("#                      #"); 
+    System.out.println("########################"); 
+    System.out.println("########################"); 
+    System.out.println("test"); 
+    
+    //test/testtest
+ }
+    
+}
